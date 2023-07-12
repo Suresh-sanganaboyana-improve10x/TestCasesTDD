1a1d2a52989c74c7823af5c5695ac2a6c26d1823
Suresh <suresh.sanganaboyana@improve10x.com>
2023-07-12 10:16:31 +0530

    Committed Unversioned files created new project

---
+++ src/com/suresh/fibonacci/Fibonacci.java
@@ -0,0 +1,15 @@
+package com.suresh.fibonacci;
+
+public class Fibonacci {
+    public int findFibonacci(int n) {
+        if (n<0) {
+            return -1;
+        } else if (n == 0) {
+            return 0;
+        } else if (n == 1) {
+            return 1;
+        } else {
+            return findFibonacci(n-1) + findFibonacci(n-2);
+        }
+    }
+}
---
+++ src/com/suresh/fibonacci/FibonacciTest.java
@@ -0,0 +1,43 @@
+package com.suresh.fibonacci;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+public class FibonacciTest {
+
+    @Test
+    public void negativeNumber() {
+        /*
+        0,1,1,2,3,5,8,13,21,34,55....
+        find -1
+        find 0
+        find 1
+        find 5
+        */
+        Fibonacci fibonacci = new Fibonacci();
+        int zero = fibonacci.findFibonacci(-2);
+        assertEquals(-1,zero);
+    }
+
+    @Test
+    public void givenZeroReturnFibZero() {
+        Fibonacci fibonacci = new Fibonacci();
+        int zero = fibonacci.findFibonacci(0);
+        assertEquals(0, zero);
+    }
+
+    @Test
+    public void givenOne_returnOne() {
+        Fibonacci fibonacci = new Fibonacci();
+        int oneThFibonacci = fibonacci.findFibonacci(1);
+        assertEquals(1, oneThFibonacci);
+    }
+
+    @Test
+    public void givenFive_returnFive() {
+        Fibonacci fibonacci = new Fibonacci();
+        int fifthFib = fibonacci.findFibonacci(10);
+        assertEquals(55, fifthFib);
+    }
+}
