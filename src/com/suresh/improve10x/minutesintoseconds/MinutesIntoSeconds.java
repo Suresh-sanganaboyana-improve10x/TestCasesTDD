594dc88cb275cec48bc78e1c24e0de37f3b86ace
Suresh <suresh.sanganaboyana@improve10x.com>
2023-07-15 11:33:18 +0530

    I worked on convert MinutesIntoSeconds in TDD process

---
+++ src/com/suresh/improve10x/minutesintoseconds/MinutesIntoSeconds.java
@@ -0,0 +1,9 @@
+package com.suresh.improve10x.minutesintoseconds;
+
+public class MinutesIntoSeconds {
+    public int convertMinutesIntoSeconds(int minutes) {
+        int seconds = 0;
+        seconds =  minutes*60;
+        return seconds;
+    }
+}
---
+++ src/com/suresh/improve10x/minutesintoseconds/MinutesIntoSecondsTest.java
@@ -0,0 +1,43 @@
+package com.suresh.improve10x.minutesintoseconds;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MinutesIntoSecondsTest {
+
+    MinutesIntoSeconds minutesIntoSeconds;
+
+    @BeforeEach
+    public void setup() {
+        minutesIntoSeconds = new MinutesIntoSeconds();
+    }
+
+    @Test
+    public void nothing() {}
+
+    @Test
+    public void givenZero() {
+        int zeroSecond = minutesIntoSeconds.convertMinutesIntoSeconds(0);
+        assertEquals(0, zeroSecond);
+    }
+
+    @Test
+    public void givenNegativeInput() {
+        int negativeNum = minutesIntoSeconds.convertMinutesIntoSeconds(-1);
+        assertEquals(-60, negativeNum);
+    }
+
+    @Test
+    public void givenOne_returnSixtySeconds() {
+        int oneMinute = minutesIntoSeconds.convertMinutesIntoSeconds(1);
+        assertEquals(60, oneMinute);
+    }
+
+    @Test
+    public void givenSixtyMinutes_returnThreeThousandSixHundred() {
+        int sixtyMinutes = minutesIntoSeconds.convertMinutesIntoSeconds(60);
+        assertEquals(3600, sixtyMinutes);
+    }
+}
