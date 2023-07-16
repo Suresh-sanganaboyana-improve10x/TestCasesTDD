fd94d5e0e6e66bb7e9647cc623e8211337f808a8
Suresh <suresh.sanganaboyana@improve10x.com>
2023-07-17 01:05:19 +0530

    I completed the ConvertAgeToDays problem with TDD methodology

---
+++ src/com/suresh/improve10x/convertagetodays/ConvertAgeToDays.java
@@ -0,0 +1,9 @@
+package com.suresh.improve10x.convertagetodays;
+
+public class ConvertAgeToDays {
+    public int getDays(int age) {
+        int days = 0;
+        days = age*365;
+        return days;
+    }
+}
---
+++ src/com/suresh/improve10x/convertagetodays/ConvertAgeToDaysTest.java
@@ -0,0 +1,43 @@
+package com.suresh.improve10x.convertagetodays;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ConvertAgeToDaysTest {
+
+    ConvertAgeToDays convertAgeToDays;
+
+    @BeforeEach
+    public void setup() {
+        convertAgeToDays = new ConvertAgeToDays();
+    }
+
+    @Test
+    public void nothing() {}
+
+    @Test
+    public void givenZero() {
+        int givenZero = convertAgeToDays.getDays(0);
+        assertEquals(0, givenZero);
+    }
+
+    @Test
+    public void givenOne() {
+        int givenOne = convertAgeToDays.getDays(1);
+        assertEquals(365, givenOne);
+    }
+
+    @Test
+    public void given23() {
+        int given23 = convertAgeToDays.getDays(23);
+        assertEquals(8395, given23);
+    }
+
+    @Test
+    public void given32() {
+        int given32 = convertAgeToDays.getDays(23);
+        assertEquals(8395, given32);
+    }
+}
