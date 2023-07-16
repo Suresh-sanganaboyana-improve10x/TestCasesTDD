51f07aec743faa9481b6cc417be9cbdbe1d7c2ad
Suresh <suresh.sanganaboyana@improve10x.com>
2023-07-16 23:28:17 +0530

    I completed the NthEvenNumber problem with TDD methodology

---
+++ src/com/suresh/improve10x/nthevennumber/NthEvenNumber.java
@@ -0,0 +1,9 @@
+package com.suresh.improve10x.nthevennumber;
+
+public class NthEvenNumber {
+    public int findNthEvenNumber(int n) {
+        int nthEvenNum = 0;
+        nthEvenNum = 2*n-2;
+        return nthEvenNum;
+    }
+}
---
+++ src/com/suresh/improve10x/nthevennumber/NthEvenNumberTest.java
@@ -0,0 +1,61 @@
+package com.suresh.improve10x.nthevennumber;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class NthEvenNumberTest {
+
+    NthEvenNumber nthEvenNumber;
+
+    @BeforeEach
+    public void setup() {
+        nthEvenNumber = new NthEvenNumber();
+    }
+
+    @Test
+    public void nothing() {}
+
+    @Test
+    public void givenZeroNthEven() {
+        int givenZero = nthEvenNumber.findNthEvenNumber(0);
+        assertEquals(0, givenZero);
+    }
+
+    @Test
+    public void givenOne() {
+        int givenOne = nthEvenNumber.findNthEvenNumber(1);
+        assertEquals(1, givenOne);
+    }
+
+    @Test
+    public void givenTwo() {
+        int givenTwo = nthEvenNumber.findNthEvenNumber(2);
+        assertEquals(2, givenTwo);
+    }
+
+    @Test
+    public void givenThree() {
+        int givenThree = nthEvenNumber.findNthEvenNumber(3);
+        assertEquals(4, givenThree);
+    }
+
+    @Test
+    public void givenNegative() {
+        int givenOne = nthEvenNumber.findNthEvenNumber(-4);
+        assertEquals(-10, givenOne);
+    }
+
+    @Test
+    public void givenTen() {
+        int givenOne = nthEvenNumber.findNthEvenNumber(10);
+        assertEquals(18, givenOne);
+    }
+
+    @Test
+    public void givenTwenty() {
+        int givenTwenty = nthEvenNumber.findNthEvenNumber(20);
+        assertEquals(38, givenTwenty);
+    }
+}
