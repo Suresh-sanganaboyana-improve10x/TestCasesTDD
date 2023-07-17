1574f48050ad646f9d70319bf4803aefddb9caea
Suresh <suresh.sanganaboyana@improve10x.com>
2023-07-17 15:55:06 +0530

    I refactor DifferenceBetweenMaxAndMinTest.java code

--- src/com/suresh/improve10x/differencemaxandmin/DifferenceBetweenMaxAndMinTest.java
+++ src/com/suresh/improve10x/differencemaxandmin/DifferenceBetweenMaxAndMinTest.java
@@ -19,32 +19,32 @@ public class DifferenceBetweenMaxAndMinTest {
     }
 
     @Test
-    public void givenZero() {
+    public void givenZero_returnZero() {
         Integer givenZero = DifferenceMaxAndMin.findDifferenceBetweenMaxAndMin(new int[] {0});
         assertEquals(0, givenZero);
     }
 
     @Test
-    public void givenNegativeNumbers() {
-        Integer givenNegativeNumbers = DifferenceMaxAndMin.findDifferenceBetweenMaxAndMin(new int[] {1,4,-50, 32,19});
+    public void givenAllNegativeNumbers_returnSeven() {
+        Integer givenAllNegativeNumbers = DifferenceMaxAndMin.findDifferenceBetweenMaxAndMin(new int[] {-2, -5, -9, -5, -4, -3});
+        assertEquals(7, givenAllNegativeNumbers);
+    }
+
+    @Test
+    public void givenNegativeAndPositiveNumbers_returnEightyTwo() {
+        Integer givenNegativeNumbers = DifferenceMaxAndMin.findDifferenceBetweenMaxAndMin(new int[] {1, 4, -50, 32, 19});
         assertEquals(82, givenNegativeNumbers);
     }
 
     @Test
-    public void givenLargeArray() {
-        Integer givenNegativeNumbers = DifferenceMaxAndMin.findDifferenceBetweenMaxAndMin(new int[] {33, 22, 66, 3, 245});
-        assertEquals(242, givenNegativeNumbers);
+    public void givenLargeArray_returnTwoHundredFourtyTwo() {
+        Integer givenLargeArray = DifferenceMaxAndMin.findDifferenceBetweenMaxAndMin(new int[] {33, 22, 66, 3, 245});
+        assertEquals(242, givenLargeArray);
     }
 
     @Test
-    public void givenBigArray() {
+    public void givenBigArray_returnBigDifference() {
         Integer givenBigArray = DifferenceMaxAndMin.findDifferenceBetweenMaxAndMin(new int[] {21, 2433, 342, 22, 66, 567, 5, 245, 99, 345, 235,2433});
         assertEquals(2428, givenBigArray);
-    }
-
-    @Test
-    public void givenAllNegativeNumbers() {
-        Integer givenAllNegativeNumbers = DifferenceMaxAndMin.findDifferenceBetweenMaxAndMin(new int[] {-2, -5, -9, -5, -4, -3});
-        assertEquals(7, givenAllNegativeNumbers);
     }
 }
