3e5f67da9c253d5a6b8b125e237feab83a125975
Suresh <suresh.sanganaboyana@improve10x.com>
2023-07-19 17:26:28 +0530

    I refactor TheFormAnimals.java for edge cases

--- src/com/suresh/improve10x/theformproblem/TheFormAnimals.java
+++ src/com/suresh/improve10x/theformproblem/TheFormAnimals.java
@@ -1,9 +1,12 @@
 package com.suresh.improve10x.theformproblem;
 
 public class TheFormAnimals {
-    public int getTotalAnimalLegs(int noOfChickenLegs, int noOfCows, int noOfPigs) {
+    public int getTotalAnimalLegs(int noOfChickens, int noOfCows, int noOfPigs) {
         int totalAnimalLegs = 0;
-        int chicken = noOfChickenLegs*2;
+        if (noOfChickens < 0 && noOfCows < 0 && noOfPigs < 0) {
+            return -1;
+        }
+        int chicken = noOfChickens*2;
         int cows = noOfCows*4;
         int pigs = noOfPigs*4;
         totalAnimalLegs = chicken+cows+pigs;
--- src/com/suresh/improve10x/theformproblem/TheFormTest.java
+++ src/com/suresh/improve10x/theformproblem/TheFormTest.java
@@ -18,12 +18,6 @@ public class TheFormTest {
     public void nothing() {}
 
     @Test
-    public void givenOneOneOne() {
-        int givenOneOneOne = theFormAnimals.getTotalAnimalLegs(1, 1, 1);
-        assertEquals(10, givenOneOneOne);
-    }
-
-    @Test
     public void givenAllZeros() {
         int givenAllZeros = theFormAnimals.getTotalAnimalLegs(0, 0, 0);
         assertEquals(0, givenAllZeros);
@@ -31,19 +25,61 @@ public class TheFormTest {
 
     @Test
     public void givenNegative() {
-        int givenAllZeros = theFormAnimals.getTotalAnimalLegs(-1, -1, -1);
-        assertEquals(-10, givenAllZeros);
+        int givenNegative = theFormAnimals.getTotalAnimalLegs(-1, -1, -1);
+        assertEquals(-1, givenNegative);
     }
 
     @Test
-    public void givenFourFiveTen() {
-        int givenAllZeros = theFormAnimals.getTotalAnimalLegs(4, 5, 10);
-        assertEquals(68, givenAllZeros);
+    public void givenChickenOne() {
+        int givenChickenOne = theFormAnimals.getTotalAnimalLegs(1, 0, 0);
+        assertEquals(2, givenChickenOne);
     }
 
     @Test
-    public void givenTwentyFourFive() {
-        int givenAllZeros = theFormAnimals.getTotalAnimalLegs(20, 4, 5);
-        assertEquals(76, givenAllZeros);
+    public void givenCowOne() {
+        int givenCowOne = theFormAnimals.getTotalAnimalLegs(0, 1, 0);
+        assertEquals(4, givenCowOne);
+    }
+
+    @Test
+    public void givenPigOne() {
+        int givenPigOne = theFormAnimals.getTotalAnimalLegs(0, 0, 1);
+        assertEquals(4, givenPigOne);
+    }
+
+    @Test
+    public void givenChickenOneCowOnePigZero() {
+        int givenChickenOneCowOnePigZero = theFormAnimals.getTotalAnimalLegs(1, 1, 0);
+        assertEquals(6, givenChickenOneCowOnePigZero);
+    }
+
+    @Test
+    public void givenChickenZeroCowOnePigOne() {
+        int givenPigOne = theFormAnimals.getTotalAnimalLegs(0, 1, 1);
+        assertEquals(8, givenPigOne);
+    }
+
+    @Test
+    public void givenOneOneOne() {
+        int givenOneOneOne = theFormAnimals.getTotalAnimalLegs(1, 1, 1);
+        assertEquals(10, givenOneOneOne);
+    }
+
+    @Test
+    public void givenTwoOneOne() {
+        int givenTwoOneOne = theFormAnimals.getTotalAnimalLegs(2, 1, 1);
+        assertEquals(12, givenTwoOneOne);
+    }
+
+    @Test
+    public void givenOneTwoOne() {
+        int givenOneTwoOne = theFormAnimals.getTotalAnimalLegs(1, 2, 1);
+        assertEquals(14, givenOneTwoOne);
+    }
+
+    @Test
+    public void givenOneOneTwo() {
+        int givenOneTwoOne = theFormAnimals.getTotalAnimalLegs(1, 1, 2);
+        assertEquals(14, givenOneTwoOne);
     }
 }
