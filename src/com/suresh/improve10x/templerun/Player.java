3fdeeeaf4e1d2c059f48b81e2427c9071068cfa5
Suresh <suresh.sanganaboyana@improve10x.com>
2023-07-22 23:43:51 +0530

    Refactor Player.java, PlayerTest.java and Coin.java, CoinTest.java

--- src/com/suresh/improve10x/templerun/Player.java
+++ src/com/suresh/improve10x/templerun/Player.java
@@ -5,6 +5,7 @@ public class Player {
     private String name;
     private int health;
     private int score;
+    private int totalCoinValue;
     public Player(String name) {
         this.name = name == null ? "" : name.trim();
         this.health = 100;
@@ -15,9 +16,8 @@ public class Player {
         this.name = name == null ? "" : name.trim();
         if (health < 0 || health > 100) {
             throw new InvalidHealthException();
-        } else {
-            this.health = health;
         }
+        this.health = health;
     }
 
     public String getName() {
@@ -33,11 +33,12 @@ public class Player {
     }
 
     public int getScore() {
+        score = totalCoinValue;
         return score;
     }
 
     public void collectCoin(Coin coin) {
-        this.score += coin.getValue();
+        this.totalCoinValue += coin.getValue();
     }
 
     public void jump() {
