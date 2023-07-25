3c60dd0e43463f551ec7b6ee3cbfb29ee46d1664
Suresh <suresh.sanganaboyana@improve10x.com>
2023-07-25 11:25:18 +0530

    I implemented Test cases for FireBallTest.java and implemented FireBall.java

---
+++ src/com/suresh/improve10x/templerun/FireBall.java
@@ -0,0 +1,18 @@
+package com.suresh.improve10x.templerun;
+
+public class FireBall {
+    private int speed;
+    public FireBall(int speed) {
+        if (speed < 0 || speed > 100) {
+            throw new InvalidFireBallSpeedException();
+        }
+        this.speed = speed;
+    }
+
+    public void roll() {
+        System.out.println("FireBall is rolled with "  + speed +  " kmh speed!");
+    }
+
+    public class InvalidFireBallSpeedException extends RuntimeException {
+    }
+}
