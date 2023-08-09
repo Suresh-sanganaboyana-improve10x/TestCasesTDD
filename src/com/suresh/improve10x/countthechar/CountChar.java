46f4b4642a5f4f9d1b8fba6bb5a382eb502ce2de
Suresh <suresh.sanganaboyana@improve10x.com>
2023-08-09 14:39:11 +0530

    I implemented CountChar.java, CountCharTest.java using TDD all test cases successfully pass

--- src/com/suresh/improve10x/countthechar/CountChar.java
+++ src/com/suresh/improve10x/countthechar/CountChar.java
@@ -1,7 +1,19 @@
 package com.suresh.improve10x.countthechar;
 
 public class CountChar {
-    public int getCharCount(String text) {
-        return 0;
+    public int getCharCount(String text, char c) {
+        if (text == null) {
+            return 0;
+        }
+        int noOfChars = 0;
+        String lowerCase = text.toLowerCase();
+        String charInString = String.valueOf(c);
+        charInString.toLowerCase();
+        for (int i = 0; i<lowerCase.length(); i++) {
+            if (c == lowerCase.charAt(i)) {
+                noOfChars++;
+            }
+        }
+        return noOfChars;
     }
 }
--- src/com/suresh/improve10x/countthechar/CountCharTest.java
+++ src/com/suresh/improve10x/countthechar/CountCharTest.java
@@ -20,8 +20,49 @@ public class CountCharTest {
 
     @Test
     public void givenEmpty_returnZero() {
-        CountChar countChar = new CountChar();
-        int givenEmpty = countChar.getCharCount("");
+        int givenEmpty = countChar.getCharCount("", 'a');
         assertEquals(0, givenEmpty);
+    }
+
+    @Test
+    public void givenNull_returnZero() {
+        int givenNull = countChar.getCharCount(null, 'a');
+        assertEquals(0, givenNull);
+    }
+
+    @Test
+    public void givenA_returnOne() {
+        int givenA = countChar.getCharCount("a", 'a');
+        assertEquals(1, givenA);
+    }
+
+    @Test
+    public void givenAB_returnOne() {
+        int givenAB = countChar.getCharCount("ab", 'b');
+        assertEquals(1, givenAB);
+    }
+
+    @Test
+    public void givenAAB_returnTwo() {
+        int givenAAB = countChar.getCharCount("aab", 'a');
+        assertEquals(2, givenAAB);
+    }
+
+    @Test
+    public void givenAAABB_returnThree() {
+        int givenAABB = countChar.getCharCount("AaAb", 'a');
+        assertEquals(3, givenAABB);
+    }
+
+    @Test
+    public void givenABB_returnTwo() {
+        int givenABB = countChar.getCharCount("Abb", 'b');
+        assertEquals(2, givenABB);
+    }
+
+    @Test
+    public void givenABBCapitalB_returnOne() {
+        int givenABB = countChar.getCharCount("ABB", 'b');
+        assertEquals(2, givenABB);
     }
 }
